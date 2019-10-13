3f12ee06e1e9e5e8013a46c41aceef6cf9433748
Student-bit <54271399+Student-bit@users.noreply.github.com>
2019-10-13 22:01:01 +1030

    Add files via upload

---
+++ SIT107_10.2D_Code_Timing.ino
@@ -0,0 +1,92 @@
+//Libraries
+#include <DHT.h>
+
+
+//Constants
+#define tempL 20
+#define tempH 28
+#define humL 20
+#define humH 50
+#define DHTPIN 2     
+#define DHTTYPE DHT22   
+DHT dht(DHTPIN, DHTTYPE);
+
+//variables
+int activ = 0; //Stores activity status 
+float temp; //Stores temperature value
+float hum;  //Stores humidity value
+
+//Functions
+
+//Light Control
+void light_con() {
+ if (activ == 1) {
+    digitalWrite(LED_BUILTIN, HIGH);
+    Serial.println("Lights turned on");
+ }
+ else {
+    digitalWrite(LED_BUILTIN, LOW);
+    Serial.println("Lights turned off");
+ }
+}
+
+//Temperature control
+void temp_con() {
+ if (activ == 1 and temp > tempH) {
+    Serial.println("Temperature high, turning on airconditioning.");
+ }
+ else if (activ == 1 and temp < tempL) {
+    Serial.println("Temperature low, turning on heating.");
+ }
+ else if (activ == 0) {
+    Serial.println("Temperature control turned off");
+ }
+}
+
+//Humidity control
+void hum_con() {
+ if (activ == 1 and hum > humH) {
+    Serial.println("Humidity high, turning on dehumidifier");
+ }
+ else if (activ == 1 and hum < humL) {
+    Serial.println("Humidity low, turning on humidifier");
+ }
+ else if (activ == 0) {
+    Serial.println("Humidity control turned off");
+ }
+}
+
+void setup() {
+    Serial.begin(9600);
+    dht.begin();
+    pinMode(3, INPUT);
+    pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop() {
+ //Variable Read
+ temp= dht.readTemperature();
+ hum = dht.readHumidity();
+
+ //Active Switch
+ if (digitalRead(3) == HIGH and activ == 0) {
+    activ = 1;
+    Serial.print("Bright system activated at ");
+    Serial.println(millis());
+    light_con();
+    temp_con();
+    hum_con();
+    delay(20000);  //delay stops the activity switch being flipped multiple times from one motion
+ }
+ 
+ else if (digitalRead(3) == HIGH and activ == 1) {
+    activ = 0;
+    Serial.print("Bright system deactivated at ");
+    Serial.println(millis());
+    light_con();
+    temp_con();
+    hum_con();
+    delay(20000); //see note above
+ }
+
+}
